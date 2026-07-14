19826e699af2024c130afa0f13e95b63326ce017
Shrishti Bansal <141552678+Shrishti2705@users.noreply.github.com>
2026-07-14 13:00:25 +0530

    new remove duplicate cod added

--- app.js
+++ app.js
@@ -77,3 +77,29 @@ console.log(newarr)
 
 
 
+// remove duplicate from same arr
+
+ let arr=[1,2,3,4,1,3,2,2]
+ 
+ for(let i=0; i<arr.length; i++){
+     for(let j=i+1; j<arr.length; j++){
+         if(arr[i]==arr[j]){
+             for(let k=j; k<arr.length-1; k++){
+                 arr[k]=arr[k+1]
+             }
+             
+         
+             arr.length--,
+              j--;
+         }
+        
+     }
+ }
+ console.log(arr)
+
+
+
+
+
+
+
